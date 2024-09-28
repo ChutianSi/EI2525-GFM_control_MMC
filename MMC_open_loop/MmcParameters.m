2a29f9d74d4c38f9a59c63bdc7ca8e54967381b7
Chutian Si (司楚天 | He/Him) <sct2357@outlook.com>
2024-09-28 16:23:58 +0200

    Unfinished PQ control

--- MMC_open_loop/MmcParameters.m
+++ MMC_open_loop/MmcParameters.m
@@ -5,28 +5,50 @@ close;
 clear;
 clc;
 
-% Nominal system frequency (Hz)
-    Fnom = 50; 
-% Number of power module per half-arm                 
-    Nb_PM = 20;
-% Nominal DC voltage of one module (V)                   
-    Vnom_PM = 1000;    
-% Nominal voltage plus DC source          
-    VDCp = (Nb_PM * Vnom_PM) / 2; 
-% Nominal voltage minus DC source  
-    VDCm = (Nb_PM * Vnom_PM) / 2;   
-% Power module capacitor (F)             
-    C_PM = 1e9; 
-% Initial capacitor DC voltage (V)               
-    Vc_Initial = Vnom_PM;  
 % SPS Simulation time step(s)      
-    Ts_Power = 10e-6;           
+Ts_Power = 5e-6;
 
-% Modulator Parameters:
+%% MMC Submodule Parameters:
+% Number of power module per half-arm                 
+Nb_PM = 20;
+% Nominal DC voltage of one module (V)                   
+Vnom_PM = 1600;    
+% Power module capacitor (F)             
+C_PM = 8e-3; 
+% Initial capacitor DC voltage (V)               
+Vc_Initial = Vnom_PM;  
+
+%% AC Grid Parameters:
+% Nominal system frequency (Hz)
+Fnom = 50; 
+% inductance (H)
+Lac = 32e-3;
+% resistance (Ohm)
+Rac = 1;
+% Norminal ph-ph voltage (V)
+Vnom_AC = (Nb_PM * Vnom_PM) / 2;
+% Rated power (VA)
+Snom = 1e6;
+%Rated power factor (/)
+cosphi = .95;
+
+%% DC Source Parameters:
+% Nominal voltage plus DC source          
+VDCp = (Nb_PM * Vnom_PM) / 2; 
+% Nominal voltage minus DC source  
+VDCm = (Nb_PM * Vnom_PM) / 2;   
+
+%% Bridge Arm Parameters:
+% inductance (H)
+L0 = 1e-3;
+% resistance (Ohm)
+R0 = .1;
+
+%% Modulator Parameters:
 % Carriers frequency (Hz)
-    Fc = Fnom * 3;       
+Fc = Fnom * 4;       
 
-% Switches parameters:
+%% Switches parameters:
 Ron = 1e-3;
 Rs = 1e6;
 Cs = inf;
